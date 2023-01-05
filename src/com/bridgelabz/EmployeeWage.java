6638c863efcde03998aa746eadead5cf7e0dc5fc
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-01-05 11:13:40 +0530

    employee wage for month

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -6,26 +6,35 @@ public class EmployeeWage {
     static final int PART_TIME_HOUR = 4;
     static final int IS_FULL_TIME = 2;
     static final int IS_PART_TIME = 1;
+    static final int WORKING_DAYS_PER_MONTH = 20;
 
     public static void main(String[] args) {
         System.out.println("welcome to Employee wage program");
-        int employeeCheck = (int) Math.floor(Math.random()*10)%3;
-        System.out.println(employeeCheck);
+        int totalWage = 0;
         int dailyWage = 0;
+        int dayCount = 1;
+        while (dayCount <=WORKING_DAYS_PER_MONTH  ) {
+              int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
+              System.out.println(employeeCheck);
 
-        switch (employeeCheck){
+        switch (employeeCheck) {
             case IS_PART_TIME:
                 dailyWage = PART_TIME_HOUR * WAGE_PER_HOUR;
-                System.out.println("Employee is part time");
+                System.out.println("Employee is part time" + dailyWage);
                 break;
             case IS_FULL_TIME:
-                 dailyWage = FULL_DAY_HOUR * WAGE_PER_HOUR;
-                 System.out.println("Employee is full time ");
-                 break;
+                dailyWage = FULL_DAY_HOUR * WAGE_PER_HOUR;
+                System.out.println("Employee is full time " + dailyWage);
+                break;
             default:
-                 System.out.println("Employee is absent");
+                System.out.println("Employee is absent");
+                break;
         }
 
-        System.out.println("Daily Employee wage is " + dailyWage);
+        totalWage = totalWage + dailyWage;
+        dayCount++;
+    }
+     System.out.println(" daily wage for day "+dayCount+" =>" + dailyWage);
+     System.out.println(" daily wage for month "+dayCount+" =>" + totalWage);
     }
 }
